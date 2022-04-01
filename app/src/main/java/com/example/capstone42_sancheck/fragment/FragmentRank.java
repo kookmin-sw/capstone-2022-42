c95c6063915618da45bd9a6b3ea71812c8bd33a2
YongUk Song <songyonguk@YongUks-MacBook-Air.local>
2022-04-01 20:29:21 +0900

    rank layout

--- app/src/main/java/com/example/capstone42_sancheck/fragment/FragmentRank.java
+++ app/src/main/java/com/example/capstone42_sancheck/fragment/FragmentRank.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,10 +15,20 @@ import com.example.capstone42_sancheck.R;
 public class FragmentRank extends Fragment {
     private View view;
 
+
+
     @Nullable
     @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         view = inflater.inflate(R.layout.frag_rank, container, false);
         return view;
+
+//        ListView listView = findViewById(R.id.listView);
+
+
+
+
     }
 }
