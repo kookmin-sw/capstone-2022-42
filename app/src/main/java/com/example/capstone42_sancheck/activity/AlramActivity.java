df2ac7ef2536685cb50cdce2bbe38571af095347
82109 <abo3323@kookmin.ac.kr>
2022-04-15 18:34:20 +0900

    2022-04-15 최신화 - 안현섭

---
+++ app/src/main/java/com/example/capstone42_sancheck/activity/AlramActivity.java
@@ -0,0 +1,16 @@
+package com.example.capstone42_sancheck.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.capstone42_sancheck.R;
+
+public class AlramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alram);
+    }
+}
